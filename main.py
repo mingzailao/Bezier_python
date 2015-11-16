4397d8dc35a7e714a56bd9181a78554e15bcf99b
mingzailao <mingzailao@163.com>
2015-11-16 13:44:12 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#!~/Pythonenv/bin/python
+# -*- coding: utf-8 -*-
 
-#!-utf8-
+
 #author:mingzailao
 #date:2015-11-12
 #Based on numpy and matplotlib almost
@@ -21,8 +23,6 @@
 
 
 #this program is based on 3-D ,the demention of the vector must be 3,the 2 demention program is under working.
-#the number of the control point can be  larger than 4 which means it can plot more than 3 times  Bezier curve.
-#the other curve ,I set the last Control Point to be the first Control Point of the first Bezier curve.
 
 from numpy import *
 import mpl_toolkits.mplot3d.axes3d as p3
@@ -39,7 +39,7 @@ def c(n,k):
 
 def getPoint():
     dim=input("please enter the dimention you want:\n")
-    n=input("please enter the number of the point\n")
+    n=4
     Points=np.zeros([n,dim])
     for i in range(n):
         Points[i]=np.asarray(input("please enter the point\n"),float)
@@ -52,26 +52,44 @@ def Bezier(Points,T):
     for j in range(T.size):
         for i in range(Points.shape[0]):
             B[j]=B[j]+Bernstein(Points.shape[0]-1,i,T[j])*Points[i]
-    return B.transpose();
-
-
-
+    return B.transpose()
+def TwoBezier(P,alpha):
+    a=P[P.shape[0]-1]+alpha*(P[P.shape[0]-1]-P[P.shape[0]-2])
+    Q=np.asarray([P[P.shape[0]-1],a,np.asarray(input("Please enter 3th Point\n")),np.asarray(input("Please Enter 4th Point\n"))])
+    return Q
 P=getPoint();
+PP=P.transpose()
 alpha=2
-a=P[P.shape[0]-1]+alpha*(P[P.shape[0]-1]-P[P.shape[0]-2])
-P_next=np.asarray([P[P.shape[0]-1],a,P[0]])
+
+
+P1=TwoBezier(P,alpha)
+PP1=P1.transpose()
+
+P2=TwoBezier(P1,alpha)
+PP2=P2.transpose()
 
 u=r_[0:1:0.001]
 x=Bezier(P,u)[0]
 y=Bezier(P,u)[1]
 z=Bezier(P,u)[2]
-x_next=Bezier(P_next,u)[0]
-y_next=Bezier(P_next,u)[1]
-z_next=Bezier(P_next,u)[2]
+x1=Bezier(P1,u)[0]
+y1=Bezier(P1,u)[1]
+z1=Bezier(P1,u)[2]
+x2=Bezier(P2,u)[0]
+y2=Bezier(P2,u)[1]
+z2=Bezier(P2,u)[2]
+
 fig=plt.figure()
 ax=p3.Axes3D(fig)
-ax.plot_wireframe(x,y,z)
-ax.plot_wireframe(x_next,y_next,z_next,color='r')
+plt.plot(PP[0],PP[1],PP[2],'mo:')
+
+ax.plot_wireframe(x,y,z,color='b')
+ax.plot_wireframe(x1,y1,z1,color='r')
+ax.plot_wireframe(x2,y2,z2,color='c')
+
+plt.plot(PP1[0],PP1[1],PP1[2],'mo:')
+plt.plot(PP2[0],PP2[1],PP2[2],'mo:')
+
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
